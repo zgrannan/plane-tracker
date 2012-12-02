8caa6c6c20348e0a43d2e8f2fbd8623e06d5b8f7
Zack Grannan <zgrannan@gmail.com>
2012-12-01 16:00:46 -0800

    Now using proper equirectangular formula

--- src/gps/GeoReference.cpp
+++ src/gps/GeoReference.cpp
@@ -12,13 +12,14 @@ double GeoReference::degToRad(double deg) {
 // Taken from: http://www.movable-type.co.uk/scripts/latlong.html
 
 vector<double> GeoReference::calculateBearingAndDistance(double lat1, double lon1, double lat2, double lon2){
-  double dy = (lat2 - lat1);
-  double dx = (cos(M_PI/180*lat1)*(lon2 - lon1));
+  double dy = (lat2 - lat1) * M_PI / 180;
+  double dx = (cos(M_PI/180*lat1)*(lon2 - lon1) * M_PI / 180);
   double angle = atan2(dy, dx);
-  double c = 2.0 * atan2(sqrt(angle),sqrt(1.0-angle));
-  double distance = RADIUS_M * c;
+  double x = M_PI / 180 * (lon2-lon1) * cos(((M_PI/360) *(lat1+lat2)));
+  double y = M_PI / 180 * (lat2-lat1);
+  double distance = sqrt(x*x+y*y) * RADIUS_M;
   vector<double> result;
-  result.push_back(-angle * 180.0 / PI);
+  result.push_back(-angle * 180 / M_PI);
   result.push_back(distance);
   return result;
 }  
--- src/test/georeferencetest.cpp
+++ src/test/georeferencetest.cpp
@@ -1,13 +1,20 @@
-#include "../gps/GeoReference.h"
+#include "src/gps/GeoReference.h"
 #include <cstdlib>
 #include <iostream>
 using namespace std;
 
 int main(int argc, char** argv){
+  if ( argc != 7 ){
+    cout << "Usage: " << argv[0] << " lat1 lon1 alt1 lat2 lon2 alt2\n";
+    return 0;
+  }
   double lat1 = atof(argv[1]);
   double lon1 = atof(argv[2]);
-  double lat2 = atof(argv[3]);
-  double lon2 = atof(argv[4]);
-  cout << lat1 << "\n" << lon1 << "\n" << lat2 << "\n" << lon2 << endl;
+  double alt1 = atof(argv[3]);
+  double lat2 = atof(argv[4]);
+  double lon2 = atof(argv[5]);
+  double alt2 = atof(argv[6]);
+  double roll, pitch;
   cout << GeoReference::calculateBearingAndDistance(lat1,lon1,lat2,lon2)[0] << endl;
+  cout << GeoReference::calculateBearingAndDistance(lat1,lon1,lat2,lon2)[1] << endl;
 }
